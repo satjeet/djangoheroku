d74c053df1352aa2e63f38a75fb870ea4d994048
satjeet <arturo.alfaro87@gmail.com>
2016-04-19 19:50:46 -0300

    seting agregado

--- natas/settings.py
+++ natas/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'natas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'natas',
+        'USER': 'satjeet',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
